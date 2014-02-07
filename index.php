cee85c2b1355accfda2a7d38debd1da94776cc3d
Alexander Bazo <alexanderbazo@googlemail.com>
2014-02-07 11:13:52 +0100

    minor changes on css

--- index.php
+++ index.php
@@ -12,14 +12,16 @@
 	</head>
 
 	<body onload="init()">
-		<h1>#Elies: <span class="sub">A study advisor dreaming of electric sheep</span></h1>
+		<h1>#Elies: <span class="sub">Ein Chatbot als Studienberater</span></h1>
 			<div id="output"></div>
 			<input id="input" type="text" name="spind" value="" placeholder="ask and press enter" onkeydown="if (event.keyCode == 13) { processInput(this.value); return false; }">
 		<footer>
-			Elise is great in helping new students. Currently she uses this <a href="./python/aiml/uni_regensburg.aiml" class="file_link" target="_new">AIML database</a> and these <a href="./prolog/facts/mi_iw.pl" class="file_link" target="_new">prolog facts</a> to answer your questions concering the study of media informatics and information sciene at the <a href="http://www.uni-regensburg.de" class="web_link" target="_new">University Regensburg</a>.
+			Elise ist ein Chatbot der Studieninteressierte und Studierende bei ihrem Studium an der Universität Regensburg unterstützt. Elise benutzt diese <a href="./python/aiml/uni_regensburg.aiml" class="file_link" target="_new">AIML-Dialogstruktur</a> und diese <a href="./prolog/facts/mi_iw.pl" class="file_link" target="_new">Prolog-Fakten</a> um Fragen rund um das Studium der Fächer <a href="http://www.iw.uni-regensburg.de" class="web_link" target="_new">Informationswissenschaft</a> und der <a href="http://www.mi.uni-regensburg.de" class="web_link" target="_new">Medieninformatik</a> an der <a href="http://www.uni-regensburg.de" class="web_link" target="_new">Universität Regensburg</a> zu benantworten.<br /><br />
+				Bei Fragen zu diesem System können Sie sich an <br /><a href="mailto:alexander.bazo@ur.de">Alexander Bazo</a>, <a href="mailto:michaela.schlesinger@stud.uni.regensburg.de">Michaela Schlesinger</a>, <a href="mailto:jessica.rak@stud.uni-regensburg.de">Jessica Rak</a> und <a href="mailto:juergen-reischer@ur.de">Jürgen Reischer</a> wenden.
 		</footer>
 
 		<a href="admin.php" id="edit_aiml">Datenbasis bearbeiten</a>
+		<a href="about.php" id="beta"></a>
 
 		<div id="course_info" class="hidden">
 			<h1><span class="course_nr">36554</span> <span class="title">Prolog</span></h1>
