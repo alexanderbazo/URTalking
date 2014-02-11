cb3ced8eb3a0efb2608fd91a20f3f6ea999c713f
Alexander Bazo <alexanderbazo@googlemail.com>
2014-02-11 15:14:02 +0100

    updated title and bot description

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 	</head>
 
 	<body onload="init()">
-		<h1>#Elise: <span class="sub">Ein Chatbot als Studienberater</span></h1>
+		<h1><span class="sub">#</span>Elise<span class="sub">: <span class="highlight">El</span>ektronisches <span class="highlight">I</span>nformations<span class="highlight">s</span>ystem für <span class="highlight">E</span>rstsemester</span></h1>
 			<div id="output"></div>
-			<input id="input" type="text" name="spind" value="" placeholder="ask and press enter" onkeydown="if (event.keyCode == 13) { processInput(this.value); return false; }">
+			<input id="input" type="text" name="spind" value="" placeholder="Stelle eine Frage und drücke die Eingabetaste" onkeydown="if (event.keyCode == 13) { processInput(this.value); return false; }">
 		<footer>
 			Elise ist ein Chatbot, der Studieninteressierte und Studierende bei ihrem Studium an der Universität Regensburg unterstützt. Elise benutzt diese <a href="./python/aiml/uni_regensburg.aiml" class="file_link" target="_new">AIML-Dialogstruktur</a> und diese <a href="./prolog/facts/mi_iw.pl" class="file_link" target="_new">Prolog-Fakten</a>, um Fragen rund um das Studium der Fächer <a href="http://www.iw.uni-regensburg.de" class="web_link" target="_new">Informationswissenschaft</a> und <a href="http://www.mi.uni-regensburg.de" class="web_link" target="_new">Medieninformatik</a> an der <a href="http://www.uni-regensburg.de" class="web_link" target="_new">Universität Regensburg</a> zu beantworten.<br /><br />
 				Bei Fragen zu diesem System können Sie sich an <br /><a href="mailto:alexander.bazo@ur.de">Alexander Bazo</a>, <a href="mailto:michaela.schlesinger@stud.uni.regensburg.de">Michaela Schlesinger</a>, <a href="mailto:jessica.rak@stud.uni-regensburg.de">Jessica Rak</a> und <a href="mailto:juergen.reischer@ur.de">Jürgen Reischer</a> wenden.
