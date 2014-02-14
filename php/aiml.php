9e7f7265775643a2f562851750c319b465552a68
Alexander Bazo <alexanderbazo@googlemail.com>
2014-02-14 08:27:05 +0100

    added simple dialog logging

--- php/aiml.php
+++ php/aiml.php
@@ -7,11 +7,14 @@
 	
 	function ask_aiml($request, $aiml_file)
 	{
+		logDialog(time(), session_id(), "human", $request);
 		$result = shell_exec('../python/ask.py -q "'.$request.'" -a "../python/aiml/'.$aiml_file.'" -s '.session_id().' -d "../python/dict/" -c "../python/lists/top10k_de.list" 2>&1');
 		$pos = strpos($result, 'WARNING');
 		if($pos === false) {
+			logDialog(time(), session_id(), "bot", $result);
 			return $result;
 		} else {
+			logDialog(time(), session_id(), "bot", "Daran hat der Autor meiner AIML-Datei nicht gedacht. Tut mir leid.");
 			return "Daran hat der Autor meiner AIML-Datei nicht gedacht. Tut mir leid.";
 		}
 		
@@ -30,6 +33,12 @@
 				return $result;
 			}
 		}
+	}
+	
+	function logDialog($timestamp, $session, $speaker, $msg) {
+		$file = fopen('../logs/'.$session.'.log', "a");
+		fwrite($file, $timestamp.",".$session.",".$speaker.",".$msg."\n");
+		fclose($file);
 	}
 
 	
